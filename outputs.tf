3450ace38cc5182861b89e2fa7d036ce8bf24cef
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-23 14:41:11 -0400

    fix

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "environment_data_all" {
   value       = data.environment_variables.all
   description = "All environment variables, derived from Epp0/environment provider"
 }
+
+output "still_ws_id" {
+  value = data.tfe_workspace.this.id
+}
