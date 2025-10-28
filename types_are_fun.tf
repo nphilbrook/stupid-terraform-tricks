088f7e7b7810c341accfeb4e000d05f01ddb1461
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-10-28 15:55:43 -0400

    Comments to help tell the story

--- types_are_fun.tf
+++ types_are_fun.tf
@@ -3,18 +3,22 @@ variable "list_variable" {
   default = []
 }
 
+# This is false
 output "empty_is_empty_right_question_mark" {
   value = var.list_variable == []
 }
 
+# This is false - more explicit demonstration of above
 output "booleans_are_fun" {
   value = tolist([]) == []
 }
 
+# STILL FALSE - list(dynamic) vs. list(string)
 output "casting_wont_save_you" {
   value = tolist([]) == var.list_variable
 }
 
+# This is the way
 output "just_use_length" {
   value = length(var.list_variable) == 0
 }
